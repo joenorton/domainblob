6fe6bb07bc4059bb1b6265ff621f4b11283f39cf
Joe Norton <joenorton75@gmail.com>
2015-10-17 22:10:24 -0700

    fixed url

--- util.rb
+++ util.rb
@@ -1,11 +1,11 @@
 ##################################################################
 # ####DomainBlob.rb -- quick domain-name lookup and idea generation
 # ####created by Joe Norton
-# ####http://softwarebyjoe.com
+# ####http://norton.io
 # #LICENSING: GNU GPLv3  License##################################
 # ! usr/bin/ruby
 def make_and_or_nav_to_dir(_thePhrase)
-  if File.directory?(RESULT_DIR_NAME)
+ if File.directory?(RESULT_DIR_NAME)
     Dir.chdir(RESULT_DIR_NAME)
  else
    Dir.mkdir(RESULT_DIR_NAME)
