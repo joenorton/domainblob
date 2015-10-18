74cd9c425ecece5cd72b81341896e618dbe8b194
Joe Norton <joenorton75@gmail.com>
2015-10-17 22:47:14 -0700

    more cleaning up

--- domainblob.rb
+++ domainblob.rb
@@ -5,7 +5,6 @@
 # #LICENSING: GNU GPLv3  License##################################
 # ! usr/bin/ruby
 require 'rubygems'
-require 'nokogiri'
 require 'whois'
 require 'open-uri'
 require 'resolv'
@@ -17,41 +16,41 @@ require_relative('util.rb')
 
 def domainblob_main
   db_opts = fetch_opts(ARGV.map(&:dup))
-  db_opts.each do |thePhrase|
-   @whoiscounter, @httpcounter, @whoisdotnetcounter = 0, 0, 0
-   @avail = []
-   start_time = Time.now
-   #
-   thePhrase = sanitize_input(thePhrase)
-   #
-   make_and_or_nav_to_dir(thePhrase)
-   #
-   blobResults = File.new(thePhrase + RESULT_FILE_EXT, 'w+')
-   #
-   start_output(blobResults, thePhrase)
-   # first we check all possible combos for the root phrase
-   get_root_domains(thePhrase)
-   # now we cycle through prefixes, then suffixes for this phrase
-   cycle_thru_all_prefix_and_suffix_lists(thePhrase)
-   #
-   avail_num = @avail.length
-   @avail = @avail.sort_by(&:length)
-   #
-   write_results(blobResults)
-   #
-   end_time = Time.now
-   time_diff = end_time - start_time
-   #
-   ending_output(time_diff, avail_num, blobResults)
-   blobResults.close
-   #
-   File.rename(
-    thePhrase + RESULT_FILE_EXT,
-    thePhrase + availNum.to_s + RESULT_FILE_EXT
-   )
-   #
-   ending_output(time_diff, avail_num)
-   Dir.chdir('..')
+  db_opts.each do |seed_keyword|
+    @whoiscounter, @httpcounter, @whoisdotnetcounter = 0, 0, 0
+    @avail = []
+    start_time = Time.now
+    #
+    seed_keyword = sanitize_input(seed_keyword)
+    #
+    make_and_or_nav_to_dir(seed_keyword)
+    #
+    f = File.new(seed_keyword + RESULT_FILE_EXT, 'w+')
+    #
+    start_output(f, seed_keyword)
+    # first we check all possible combos for the root phrase
+    get_root_domains(seed_keyword)
+    # now we cycle through prefixes, then suffixes for this phrase
+    cycle_thru_all_prefix_and_suffix_lists(seed_keyword)
+    #
+    avail_num = @avail.length
+    @avail = @avail.sort_by(&:length)
+    #
+    write_results(f)
+    #
+    end_time = Time.now
+    time_diff = end_time - start_time
+    #
+    ending_output(time_diff, avail_num, f)
+    f.close
+    #
+    File.rename(
+      seed_keyword + RESULT_FILE_EXT,
+      seed_keyword + availNum.to_s + RESULT_FILE_EXT
+    )
+    #
+    ending_output(time_diff, avail_num)
+    Dir.chdir('..')
   end
 end
 domainblob_main
